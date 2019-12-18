bc82c868fdaaee99ac00f50b91ec05de157dec4e
Me Gilang R <megilangr1@gmail.com>
2019-12-18 16:50:27 +0700

    Update

--- resources/views/frontend/layout/header.blade.php
+++ resources/views/frontend/layout/header.blade.php
@@ -1,21 +1,13 @@
 <div class="row mb-2">
-    <div class="col-sm-4 col-md-4 text-center">
+    <div class="col-sm-4 col-md-4 text-center p-0 m-0">
 			<a href="{{ route('frontend.main') }}">
-				<img src="{{ asset('images/main-logo.png') }}" alt="Logo-Utama" class="img-fluid" style="height: 90px;" >
+				<img src="{{ asset('images/bacapaja-logo.png') }}" alt="Logo-Utama" class="img-fluid" style="height: 90px;" >
 			</a>	
 		</div>
     <div class="col-sm-8 col-md-8 pr-2 text-center d-none d-sm-none d-md-block d-lg-block">
         <img src="{{ asset('images/iklan-01.png') }}" alt="Iklan-01" class="img-fluid" style="height: 90px;">
     </div>
     <div class="col-sm-12 col-md-12 mt-4 mb-0">
-				<div class="col-md-12 text-center mb-2">
-						<div class="p-2 mb-2 bg-light">
-								<h4 class="font">Bacapaja.xyz</h4>
-								<p class="mb-0">
-										<marquee>Selamat Datang di-Bacapaja. Baca Apa Aja, Belajar Apa Aja, Pengetahuan Apa Aja.</marquee>
-								</p>
-						</div>
-				</div>		
         {{-- <div class="input-group">
             <span class="input-group-text m-0" style="border-radius: 0px;" >
                 BACAPAJA.XYZ
